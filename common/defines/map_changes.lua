09a2e118e29e125159f0d86741769682d652aa55
ZhiZhongD <178995264@qq.com>
2024-09-07 16:31:22 +0800

    创建mod

---
+++ common/defines/map_changes.lua
@@ -0,0 +1,44 @@
+-- NDefines.NGraphics.CITY_SPRAWL_SHRINK_DISTANCE = 22000.0 			-- Start shrinking at this distance
+-- NDefines.NGraphics.CITY_SPRAWL_DRAW_DISTANCE = 250000.0 				-- Remove at this distance
+-- NDefines.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250000.0					-- Remove map objects at this distance
+
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 5 -- thickness in pixels 0
+NDefines.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 2 -- The center gradient is linear 1/255 per pixel for this many pixels.  This is the countries color opacity.  
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 5  -- How blurry country borders are.  Less is finer.  
+
+NDefines.NGraphics.BORDER_WIDTH = 1
+
+NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_COUNTRY = 0.15 -- 0.99 is borders only, 0.01 is full coverage
+
+NDefines.NGraphics.DRAW_COUNTRY_NAMES_CUTOFF = 400.0
+NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 200.0 			-- Remove province names beyond this distance
+NDefines.NGraphics.MAP_MODE_TERRAIN_TRANSPARENCY = 0.5
+NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_TERRAIN = 0.1
+
+NDefines.NGraphics.STATE_BORDER_FADE_NEAR = 400
+NDefines.NGraphics.STATE_BORDER_FADE_FAR = 500
+
+NDefines.NGraphics.BORDER_COLOR_SELECTION_STATE_R = 0.8
+NDefines.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.8
+NDefines.NGraphics.BORDER_COLOR_SELECTION_STATE_B = 1
+NDefines.NGraphics.BORDER_COLOR_SELECTION_STATE_A = 1.0
+
+NDefines.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 1.0
+NDefines.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 0.8
+NDefines.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 1
+NDefines.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_A = 1.0
+
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 10.0
+
+
+
+NDefines.NGraphics.MOON_HEIGHT = 600 -- higher means softer shadows and more intense light
+NDefines.NGraphics.SUN_HEIGHT  = 600 -- higher means softer shadows and more intense light
+NDefines.NGraphics.SUN_LATITUDE = 100 --usually 848
+NDefines.NGraphics.SUN_INTENSITY = 1.0 -- 0.4 usually 1
+NDefines.NGraphics.MOON_INTENSITY = 1.0 --Usually 2.5
+NDefines.NGraphics.BLOOM_SCALE = 0.0
+
+NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 300.0	--camera distance at which the icons begin to group up	
+NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 600.0	--Camera distance at which the icons begin to group up on state level	
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 600.0	--second camera distance at which the icons begin to group up
---
+++ descriptor.mod
@@ -0,0 +1,6 @@
+version="0.1"
+tags={
+	"Map"
+}
+name="BFD"
+supported_version="1.14.8"
